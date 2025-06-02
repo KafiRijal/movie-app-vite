a1a4460d727f9e09240cf797e28ac665230881f4
KafiRijal <kafirs75@gmail.com>
2025-06-02 10:13:40 +0700

    Tugas 4

--- src/components/UI/Button/Index.jsx
+++ src/components/UI/Button/Index.jsx
@@ -1,24 +1,24 @@
 import styled, { css } from "styled-components";
 
-const colors = {
-  primary: "#4361ee",
-  secondary: "#b5179e",
-};
+// const colors = {
+//   primary: "#4361ee",
+//   secondary: "#b5179e",
+// };
 
-const sizes = {
-  sm: {
-    fontSize: "0.8rem",
-    padding: "0.2rem 0.5rem",
-  },
-  md: {
-    fontSize: "1rem",
-    padding: "0.5rem 1rem",
-  },
-  lg: {
-    fontSize: "1.3rem",
-    padding: "0.5rem 1rem",
-  },
-};
+// const sizes = {
+//   sm: {
+//     fontSize: "0.8rem",
+//     padding: "0.2rem 0.5rem",
+//   },
+//   md: {
+//     fontSize: "1rem",
+//     padding: "0.5rem 1rem",
+//   },
+//   lg: {
+//     fontSize: "1.3rem",
+//     padding: "0.5rem 1rem",
+//   },
+// };
 
 const Button = styled.button`
   padding: 0.8 rem 2rem;
@@ -27,10 +27,13 @@ const Button = styled.button`
   color: #fff;
   cursor: pointer;
 
-  background-color: ${({ variant }) => colors[variant] || colors["primary"]};
+  background-color: ${({ variant, theme }) =>
+    theme.colors[variant] || theme.colors.primary};
 
-  font-size: ${({ size }) => sizes[size || "md"].fontSize};
-  padding: ${({ size }) => sizes[size || "md"].padding};
+  ${({ size = "md", theme }) => css`
+    font-size: ${theme.button[size].fontSize};
+    padding: ${theme.button[size].padding};
+  `}
 
   ${(props) =>
     props.full &&
